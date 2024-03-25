6ddb7581fa27eb41feccbe0f31beab07857472c7
Artemy <jevlakovartemy@yandex.ru>
2024-03-25 17:25:30 +0300

    Добавить изображение и сохранение после логина, и заглушку функции profile

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ from data.db_session import global_init, create_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
+MAIN_USER = None
 
 
 @app.route("/astronomy-site")
 def astronomy_site():
-    return render_template("main_page.html", title="astronomy-site")
+    return render_template("main_page.html", title="astronomy-site", user=MAIN_USER)
+
+
+@app.route("/astronomu-site/profile")
+def profile():
+    pass
 
 
 @app.route("/astronomy-site/astronomy_sign_in", methods=['GET', 'POST'])
 def astronomy_sign_in():
+    global MAIN_USER
     form = SignIn()
     if form.validate_on_submit():
         global_init("db/astronomy_site_users.db")
@@ -22,6 +29,7 @@ def astronomy_sign_in():
         user = session.query(User).filter(
             User.username == form.username.data).first()
         if user and user.check_password(form.password.data):
+            MAIN_USER = user
             return redirect(url_for('astronomy_site'))
         else:
             return render_template("sign_in.html", form=form, message='Incorrect password.', title='Sign in')
@@ -30,6 +38,7 @@ def astronomy_sign_in():
 
 @app.route("/astronomy-site/astronomy_sign_up", methods=['GET', 'POST'])
 def astronomy_sign_up():
+    global MAIN_USER
     form = SignUp()
     if form.validate_on_submit():
         if form.password.data != form.repeat_password.data:
@@ -44,6 +53,7 @@ def astronomy_sign_up():
         user.set_password(form.password.data)
         session.add(user)
         session.commit()
+        MAIN_USER = user
         return redirect(url_for("astronomy_site"))
     return render_template('sign_up.html', form=form, title='Sign up')
 
